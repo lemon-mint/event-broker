7a745321c09932da275f45513fd28f2b7c6db0b4
lemon-mint <55233766+lemon-mint@users.noreply.github.com>
2021-01-08 16:04:45 +0900

    wss

--- public/index.js
+++ public/index.js
@@ -1,18 +1,22 @@
 function connect() {
-    let ws = new WebSocket("ws://"+window.location.host+"/ws/343677762813eaeb65704cc8d9e96f7a444ba0cca92ff861af7f68648b3e6ef1");
+    let proto = "ws";
+    if (window.location.protocol == "https:") {
+        proto = "wss";
+    }
+    let ws = new WebSocket(proto + "://" + window.location.host + "/ws/343677762813eaeb65704cc8d9e96f7a444ba0cca92ff861af7f68648b3e6ef1");
     let heartbeatID = "";
-    ws.onmessage = (e)=>{
+    ws.onmessage = (e) => {
         const data = JSON.parse(e.data);
         console.log(data);
         if (data.event === "reloadpage") {
             window.location.reload();
         }
     };
-    ws.onclose = ()=>{
-        setTimeout(connect,1000);
+    ws.onclose = () => {
+        setTimeout(connect, 1000);
         clearInterval(heartbeatID);
     };
-    ws.onopen = ()=>{
+    ws.onopen = () => {
         heartbeatID = setInterval(() => {
             if (ws.readyState == WebSocket.OPEN) {
                 ws.send(":heartbeat");
